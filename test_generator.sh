0d87e40e95720dba4bda2d2cc77b1e83a8a4f469
mibes <mibes@avaya.com>
2020-03-20 16:33:25 +0100

    Release build for generator test

--- test_generator.sh
+++ test_generator.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
+if [ ! -f ./target/release/xsd_gen ]; then
+  cargo build --release
+fi
 
-./target/debug/xsd_gen -p resources/smgr -i agentCommProfile.xsd -o examples/generated/smgr.rs
+./target/release/xsd_gen -p resources/smgr -i agentCommProfile.xsd -o examples/generated/smgr.rs
+./target/release/xsd_gen -p resources/aic -i agent_wsdl.xml -o examples/generated/aic.rs
