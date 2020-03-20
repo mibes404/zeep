c90dbaf0f84794c149606fa0456904421a8b74f1
mibes <mibes@avaya.com>
2020-03-20 15:27:12 +0100

    Input parameters

--- src/main.rs
+++ src/main.rs
@@ -22,22 +22,40 @@ fn main() {
         .about("Generates Serde annotated Rust structs from XSD")
         .arg(
             Arg::with_name("to_file")
-                .short("f")
-                .long("file")
+                .short("o")
+                .long("output")
                 .takes_value(true)
                 .help("Output to file"),
+        )
+        .arg(
+            Arg::with_name("from_file")
+                .short("i")
+                .long("input")
+                .takes_value(true)
+                .help("Input from XSD/WSDL file"),
+        )
+        .arg(
+            Arg::with_name("path")
+                .short("p")
+                .long("path")
+                .takes_value(true)
+                .help("Base path for the XSD file(s)"),
         )
         .get_matches();
 
     let to_file_name = matches.value_of("to_file");
+    let from_file_name = matches
+        .value_of("to_file")
+        .unwrap_or("agentCommProfile.xsd");
+    let base_path = matches.value_of("path").unwrap_or("resources/smgr");
 
     if let Some(file_name) = to_file_name {
         let mut file = File::create(file_name).expect("can not create file");
         let mut writer = NodeWriter::new_file(file);
-        writer.process_file("resources/smgr", "agentCommProfile.xsd");
+        writer.process_file(base_path, from_file_name);
     } else {
         let mut writer = NodeWriter::default();
-        writer.process_file("resources/smgr", "agentCommProfile.xsd");
+        writer.process_file(base_path, from_file_name);
     }
 }
 
