30810a7d688260bdc8540121c2b770631a20286f
mibes <mibes@avaya.com>
2020-03-24 07:52:08 +0100

    Use macro to wrap message in evelope

--- soap_client/src/soap.rs
+++ soap_client/src/soap.rs
@@ -4,28 +4,31 @@ use yaserde::{YaDeserialize, YaSerialize};
 
 const SOAP_ENVELOPE: &str = "http://www.w3.org/2003/05/soap-envelope/";
 
-#[derive(Debug, Default, YaSerialize, YaDeserialize)]
-#[yaserde(
-    rename = "Envelope",
-    namespace = "soap: http://www.w3.org/2003/05/soap-envelope/"
-)]
-struct Envelope {
-    #[yaserde(rename = "encodingStyle", prefix = "soap", attribute)]
-    encoding_style: String,
-    #[yaserde(rename = "tns", prefix = "xmlns")]
-    tnsattr: String,
-    #[yaserde(rename = "urn", prefix = "xmlns")]
-    urnattr: String,
-    #[yaserde(rename = "xsi", prefix = "xmlns")]
-    xsiattr: String,
-    #[yaserde(rename = "Header", prefix = "soap")]
-    header: Message,
-    #[yaserde(rename = "Body", prefix = "soap")]
-    body: Message,
+macro_rules! envelop {
+    ($type: ty) => {
+        #[derive(Debug, Default, YaSerialize, YaDeserialize)]
+        #[yaserde(
+            rename = "Envelope",
+            namespace = "soap: http://www.w3.org/2003/05/soap-envelope/"
+        )]
+        struct Envelope {
+            #[yaserde(rename = "encodingStyle", prefix = "soap", attribute)]
+            encoding_style: String,
+            #[yaserde(rename = "Header", prefix = "soap")]
+            header: Header,
+            #[yaserde(rename = "Body", prefix = "soap")]
+            body: $type,
+        }
+    };
 }
 
 #[derive(Debug, Default, YaSerialize, YaDeserialize)]
 struct Message {}
+
+#[derive(Debug, Default, YaSerialize, YaDeserialize)]
+struct Header {}
+
+envelop!(Message);
 
 #[cfg(test)]
 mod tests {
@@ -35,10 +38,7 @@ mod tests {
     fn test_envelope() {
         let e = Envelope {
             encoding_style: "http://www.w3.org/2003/05/soap-encoding".to_string(),
-            tnsattr: "".to_string(),
-            urnattr: "".to_string(),
-            xsiattr: "".to_string(),
-            header: Message {},
+            header: Header {},
             body: Message {},
         };
 
