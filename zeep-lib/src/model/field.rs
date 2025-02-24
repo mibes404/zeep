55efc3bcf9626b0fefee83fcf00d40315aa62c48
Marcel <mibes@avaya.com>
2025-02-24 19:33:45 +0100

    fix: enhance optional and vector handling in field.rs

--- zeep-lib/src/model/field.rs
+++ zeep-lib/src/model/field.rs
@@ -41,13 +41,17 @@ impl<'n> TryFromNode<'n> for Field {
 
         let is_attribute = node.tag_name().name() == "attribute";
 
+        let parent_is_optional = node.parent().and_then(|n| n.attribute("minOccurs")) == Some("0");
+
         let is_optional = if is_attribute {
             node.attribute("use") != Some("required")
         } else {
-            node.attribute("minOccurs") == Some("0")
+            node.attribute("minOccurs") == Some("0") || parent_is_optional
         };
 
-        let is_vec = Node::attribute(&node, "maxOccurs") == Some("unbounded");
+        let parent_is_vec = node.parent().and_then(|n| n.attribute("maxOccurs")) == Some("unbounded");
+        let is_vec = Node::attribute(&node, "maxOccurs") == Some("unbounded") || parent_is_vec;
+
         let is_choice = node.parent().is_some_and(|n| n.tag_name().name() == "choice");
 
         // check if this is an any type
